c0f857ec8fada9ab5bd201cdaa4c35c2423a2336
ban061 <martinban97@gmail.com>
2022-09-18 20:39:16 +0200

    Fix bugs

--- main.cpp
+++ main.cpp
@@ -5,27 +5,28 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // const typo
-    int* b = new int[NELEMENTS];
+    int* b = new int[N_ELEMENTS];
     // hianyzo ';' es "-re csere
-    std::cout << '1-100 ertekek duplazasa'
-        // for ciklus kijavitasa
-        for (int i = 0;)
-        {
-            b[i] = i * 2;
-        }
+    std::cout << "1-100 ertekek duplazasa\n";
     // for ciklus kijavitasa
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        b[i] = i * 2;
+    }
+    // for ciklus kijavitasa
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         // ertek kiirasa
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    // init atlag
+    int atlag = 0;
     // simma vesszo pontosra javitasa
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         // hianyzo ";"
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
