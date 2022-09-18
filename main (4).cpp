7d0fd88b9de925cf96b7697513c66c51bc372b7d
ban061 <martinban97@gmail.com>
2022-09-18 20:22:27 +0200

    Revert "Rename and comments"
    This reverts commit 52c26cb2548847cf7ee7eb9f62ca844d97a3b5cc.

--- main (4).cpp
+++ main (4).cpp
@@ -4,27 +4,20 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // const typo
-    int* b = new int[NELEMENTS];
-    // hianyzo ';' es "-re csere
+    int *b = new int[NELEMENTS];
     std::cout << '1-100 ertekek duplazasa'
-        // for ciklus kijavitasa
-        for (int i = 0;)
-        {
-            b[i] = i * 2;
-        }
-    // for ciklus kijavitasa
+    for (int i = 0;)
+    {
+        b[i] = i * 2;
+    }
     for (int i = 0; i; i++)
     {
-        // ertek kiirasa
         std::cout << "Ertek:"
-    }
+    }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    // simma vesszo pontosra javitasa
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        // hianyzo ";"
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
