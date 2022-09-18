7c4e866991b91dd5a0558bccf35d1df40fbdcf9b
ban061 <martinban97@gmail.com>
2022-09-18 20:24:55 +0200

    Rename and comments

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    // const typo
+    int* b = new int[NELEMENTS];
+    // hianyzo ';' es "-re csere
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }
+        // for ciklus kijavitasa
+        for (int i = 0;)
+        {
+            b[i] = i * 2;
+        }
+    // for ciklus kijavitasa
     for (int i = 0; i; i++)
     {
+        // ertek kiirasa
         std::cout << "Ertek:"
-    }    
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
+    // simma vesszo pontosra javitasa
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        // hianyzo ";"
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
